3795ae49c3693b34840aa6718ad41b65047f93f3
SD <ss244@hw.ac.uk>
2018-08-16 21:13:35 +0100

    Save the most recent directory path of the selected file

--- src/JavaNs2/Models/FileLoader.java
+++ src/JavaNs2/Models/FileLoader.java
@@ -5,12 +5,12 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 
 public class FileLoader {
+    private String defaultPath = "";
 
     public File loadFile() {
         //TODO: change the fixed file path, maybe remember the most recent file path
 //        JFileChooser fileChooser = new JFileChooser("/Users/SD/Desktop/School's Lectures/Final Project/Examples/a2_AODV_new.tr"); // use for testing
-        JFileChooser fileChooser = new JFileChooser();
-//        JFileChooser fileChooser = new JFileChooser();
+        JFileChooser fileChooser = new JFileChooser(defaultPath);
         fileChooser.setAcceptAllFileFilterUsed(false);
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Ns2 Trace Files Only", "tr");
         fileChooser.addChoosableFileFilter(filter);
@@ -22,28 +22,10 @@ public class FileLoader {
             String directory_path = selectedFile.getParent();   // returns directory path
             String fileName = selectedFile.getName();  // file's name
 //            System.out.println(selectedFile.getName() + " \n" + selectedFile_path + "\n" + directory_path + "\n");
+            defaultPath = directory_path;
             return selectedFile;
         } else {
             return null;
         }
     }
-
-//    boolean fileSelect(String Filename, String FileName_Complete_path, String Directory_path) throws IOException
-//    {
-//        this.Filename = Filename;
-//        FileSelected = true;
-//        // JOptionPane.showMessageDialog(null,"Yes");
-//        // String fileNameWithOutExt = "Tests.xml".replaceFirst("[.][^.]+$", "");
-//        String fileNameWithOutExt = Filename.replaceFirst("[.][^.]+$", "");
-//        this.inputFileName = FileName_Complete_path;
-//        if (newFile == true) {
-//            //System.out.println("SequeValue"+SequeValue);
-//            this.outputFileName = Directory_path + "\\" + fileNameWithOutExt + "_" + SequeValue + "_" + ".txt";
-//        }
-//
-//        fout = new FileWriter(outputFileName, true); //the true will append the new data
-//        op = new BufferedWriter(fout);
-//
-//        return FileSelected;
-//    }
 }
